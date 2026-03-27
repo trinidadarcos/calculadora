7a531dc664fe448445393df750c1472a7670defc
Trinidad Arcos <trinidadarcos@MacBook-Air-de-Trinidad.local>
2026-03-27 10:03:36 -0300

    Agregar servidor para Railway

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
